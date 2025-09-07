5bb2025746ddb109bcbfd6b78ed3c8d893002971
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-09-07 21:43:06 +0530

    Error fixed

--- src/app/pages/landing.tsx
+++ src/app/pages/landing.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from '../components/navbar'
 import Footer from "../components/footer" 
 
 function Landing() {
   return (
     <>
-      <Navbar />
       <Footer />
     </>
   )
